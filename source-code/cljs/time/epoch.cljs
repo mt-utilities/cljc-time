6d07d185a228ae7299e89f92f86a7a89d8f802fa
Bit Handshake <adam@monotech.hu>
2022-10-18 15:01:57 +0200

    t1.3.2 #1 - initial commit

---
+++ source-code/cljs/time/epoch.cljs
@@ -0,0 +1,45 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns time.epoch
+    (:require [time.converters :as converters]))
+
+
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(defn epoch-ms
+  ; @return (ms)
+  [])
+  ; TODO
+
+(defn epoch-s
+  ; @return (s)
+  [])
+  ; TODO
+
+(defn epoch-ms->timestamp-string
+  ; @param (ms) n
+  ;
+  ; @example
+  ;  (time/epoch-ms->timestamp-string 1640800860000)
+  ;  =>
+  ;  "2021-12-29T18:01:00.000Z"
+  ;
+  ; @return (string)
+  [n])
+  ; TODO
+
+(defn epoch-s->timestamp-string
+  ; @param (s) n
+  ;
+  ; @example
+  ;  (time/epoch-s->timestamp-string 1640800860)
+  ;  =>
+  ;  "2021-12-29T18:01:00.000Z"
+  ;
+  ; @return (string)
+  [n])
+  ; TODO
---
+++ source-code/cljs/time/schedule.cljs
@@ -0,0 +1,39 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns time.schedule)
+
+
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(defn set-timeout!
+  ; @param (function) f
+  ; @param (ms) timeout
+  ;
+  ; @usage
+  ;  (time/set-timeout! #(println "3 sec") 3000)
+  ;
+  ; @return (integer)
+  [f timeout]
+  (.setTimeout js/window f timeout))
+
+(defn set-interval!
+  ; @param (function) f
+  ; @param (ms) interval
+  ;
+  ; @usage
+  ;  (time/set-interval! #(println "3 sec") 3000)
+  ;
+  ; @return (integer)
+  [f interval]
+  (.setInterval js/window f interval))
+
+(defn clear-interval!
+  ; @param (integer) interval-id
+  ;
+  ; @return (nil)
+  [interval-id]
+  (.clearInterval js/window interval-id))
